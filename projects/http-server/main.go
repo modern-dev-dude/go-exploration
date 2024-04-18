9b05fed160e8c8c71c72e92201a967ac6c47a57e
Jason S <jstodmdd@amazon.com>
2024-04-17 22:07:41 -0400

    added html template

--- projects/http-server/main.go
+++ projects/http-server/main.go
@@ -1,37 +1,58 @@
 package main
 
 import(
-	"errors"
-	"fmt"
+	"log"
 	"net/http"
-	"net/http/httputil"
-	"io"
+	"html/template"
 	"os"
 )
 
-func getPage (res http.ResponseWriter, req *http.Request){
-	fmt.Printf("got / request\n")
+type PageData struct {
+	Title string
+	Message string
+} 
 
-	requestDump, err := httputil.DumpRequest(req, true)
-	fmt.Println(string(requestDump))
-	
-	if err != nil {
-  	fmt.Println(err)
-	}
-
-	template := "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" /><title>Document</title></head><body><p>Some text</p></body></html>"
-
-	io.WriteString(res, template)
-}
 
 func main() {
-	http.HandleFunc("/", getPage)
+	http.HandleFunc("/", handler)
 
+	log.Println("Starting server")
+	err := http.ListenAndServe(":8080", nil)
 
-	if errors.Is(err, http.ErrServerClosed){
-		fmt.Printf("server closed\n")
-	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
+	if err != nil {
+		log.Fatalf("Server failed to load %v", err)
+	}
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+	data := PageData{
+		Title: "Go Template with HTMX",
+		Message: "Hello from the server",
+	}
+
+	dir, err := os.Getwd()
+	if err != nil{
+		log.Fatalf("Error getting current working directory: %v", err)
+	}
+
+	pathToIndexHtml := dir + "/public/index.html"
+
+	log.Println("Current working directory:", pathToIndexHtml)
+
+	tmpl, err := template.ParseFiles(pathToIndexHtml)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.ExecuteTemplate(os.Stdout,pathToIndexHtml, data); err != nil {
+		log.Printf("Error executing template: %v", err)
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
