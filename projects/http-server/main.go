c9b324ba3ce022d969cdfc8fee7a0b3aaf2a0cfa
Jason S <jstodmdd@amazon.com>
2024-04-18 21:40:50 -0400

    add pokemon template and data fetching

--- projects/http-server/main.go
+++ projects/http-server/main.go
@@ -7,16 +7,40 @@ import(
 	"os"
 	"server/pkg/file-watcher"
 	"encoding/json"
+	"math/rand/v2"
+	"strconv"
+	"io/ioutil"
+	"strings"
 )
 
 type PageData struct {
 	Title string
 } 
 
-type PokemonData struct {
-	PokeName string
-	PokeHp string
+type Cries struct {
+		Latest string
+		Legacy string
 }
+
+type PokemonData struct {
+	Name string
+	FrontDefault string
+	Cries Cries 
+}
+
+type Pokemon struct {
+	ID  int `json:"id"`
+	Name  string `json:"name"`
+	Sprites  struct {
+		FrontDefault string `json:"front_default"`
+	} `json:"sprites"`
+	Cries struct {
+		Latest string `json:"latest"`
+		Legacy string `json:"legacy"`
+	} `json:"cries"`
+}
+
+var getPokimonByIdxUrl string = "https://pokeapi.co/api/v2/pokemon/"
 
 
 func main() {
@@ -36,32 +60,56 @@ func main() {
 }
 
 func getPokemonHandler(w http.ResponseWriter, r *http.Request){
-	pokemon := PokemonData()
 
-	res := map[string]interface{}{
-		"PokeName" : "Pickachu",
-		"PokeHp" : "50",
-	}
+	// generate random idx
 
-	pokemon.PokeHp = res["PokeHp"].(string)
-	pokemon.PokeName = res["PokeName"].(string)
+	randPokeUrl := getPokimonByIdxUrl + strconv.Itoa(rand.IntN(152))
+	log.Println(randPokeUrl)
 
-	jsonResponse, err := json.Marshal(pokemon)
-	if err != nil{
-		log.Printf("Error marshaling JSON: %v\n", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
+	res, err := http.Get(randPokeUrl)
 
-	w.Header().Set("Content-type", "application/json")
-
-	_, err = w.Write(jsonResponse)
 	if err != nil {
-		log.Printf("Error writing response: %v\n", err)
+		log.Printf("Error getting pokemon: %v\n", err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 
+	responseData, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Printf("Error reading pokemon response: %v\n", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	var pokemonData Pokemon
+	json.Unmarshal(responseData, &pokemonData)
+	pokemon := PokemonData{
+		strings.Title(pokemonData.Name), 
+		pokemonData.Sprites.FrontDefault, 
+		Cries{
+			Latest:pokemonData.Cries.Latest, 
+			Legacy:pokemonData.Cries.Legacy,
+		},
+	}
+
+	dir, err := os.Getwd()
+	if err != nil{
+		log.Fatalf("Error getting current working directory: %v", err)
+	}
+
+	pathToIndexHtml := dir + "/public/pokemon.html"
+
+	tmpl, err := template.ParseFiles(pathToIndexHtml)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, pokemon)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request){
